ecbc6c882280116da9d1a5a75361651ab8e9bec1
ayu <133560587+ayu10293@users.noreply.github.com@users.noreply.github.com>
2023-06-12 04:07:43 +0000

    hozon

--- db/migrate/20230607124443_create_lists.rb
+++ db/migrate/20230607124443_create_lists.rb
@@ -1,9 +1,10 @@
-class CreateLists < ActiveRecord::Migration[6.1]
+class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
+      # 以下の2行を追加
       t.string :title
       t.string :body
-
+      t.string :author
       t.timestamps
     end
   end
